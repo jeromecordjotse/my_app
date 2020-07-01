d4f5898b5cfeca667a6d0517edc21b965478b0c6
jecordjotse <jeromecordjotse@gmail.com>
2020-07-01 06:23:48 +0000

    Updates

---
+++ precache-manifest.c06d60fa84a4a5c3ee063cfccdfaca91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc0db055c56cac0fbb7f0c3b43c69d2",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b32f10c90ebcf9b6e8e0",
+    "url": "/my-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "57fb081d93ef2779cf53",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b32f10c90ebcf9b6e8e0",
+    "url": "/my-app/static/js/2.524548fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.524548fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57fb081d93ef2779cf53",
+    "url": "/my-app/static/js/main.8bc9cd6b.chunk.js"
+  },
+  {
+    "revision": "62eb3229b9f2cacc5fbd",
+    "url": "/my-app/static/js/runtime-main.3c93dadc.js"
+  }
+]);
